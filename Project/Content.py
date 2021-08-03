6f318a06e6f676daec97b59c579e46f01154eeb1
ShirAvneri <shiravneri@gmail.com>
2021-08-03 14:56:19 +0300

    Added notes numbers

--- Project/Content.py
+++ Project/Content.py
@@ -13,36 +13,33 @@ class Content(QWidget):
         self.setObjectName(self.name)
         self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
         self.setStyleSheet("QWidget#" + self.name + " { " + self.style + " }")
-        self.classical_guitar_tuner_content()
+        self.classical_guitar_tuner()
 
-    def classical_guitar_tuner_content(self):
+    def classical_guitar_tuner(self):
         guitar_image_label = QLabel(self)
         guitar_image_label.setObjectName("GuitarImageLabel")
         guitar_image_label.setGeometry(QRect(170, 50, 260, 500))
         guitar_image_label.setStyleSheet("QLabel#GuitarImageLabel { border-image: url(ClassicGuitarTuningPng.png) "
                                          "0 0 0 stretch stretch; }")
-        d = GuitarTuningButton(self, "D", 100, 115)
-        d.set_button()
-        a = GuitarTuningButton(self, "A", 100, 175)
-        a.set_button()
-        low_e = GuitarTuningButton(self, "E", 100, 235, True)
-        low_e.set_button()
-        g = GuitarTuningButton(self, "G", 450, 115)
-        g.set_button()
-        b = GuitarTuningButton(self, "B", 450, 175)
-        b.set_button()
-        high_e = GuitarTuningButton(self, "E", 450, 235)
-        high_e.set_button()
+        d3 = GuitarTuningButton(self, "D3", 100, 115)
+        d3.set_button()
+        a2 = GuitarTuningButton(self, "A2", 100, 175)
+        a2.set_button()
+        e2 = GuitarTuningButton(self, "E2", 100, 235)
+        e2.set_button()
+        g3 = GuitarTuningButton(self, "G3", 450, 115)
+        g3.set_button()
+        b3 = GuitarTuningButton(self, "B3", 450, 175)
+        b3.set_button()
+        e4 = GuitarTuningButton(self, "E4", 450, 235)
+        e4.set_button()
 
 
 class GuitarTuningButton(QPushButton):
-    def __init__(self, parent, note, x_pos, y_pos, is_low=False):
+    def __init__(self, parent, note, x_pos, y_pos):
         super().__init__(parent)
         self.note = note
-        if is_low:
-            self.name = note + "_low_note"
-        else:
-            self.name = note + "_note"
+        self.name = note + "_note"
         self.setObjectName(self.name)
         self.x = x_pos
         self.y = y_pos
