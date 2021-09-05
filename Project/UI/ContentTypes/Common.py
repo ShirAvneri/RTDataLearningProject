7578387d34c1bb453f22a2145650afd32ffb7035
hadarmanor12 <50790257+hadarmanor12@users.noreply.github.com>
2021-09-05 13:22:38 +0300

    add recording page

--- Project/UI/ContentTypes/Common.py
+++ Project/UI/ContentTypes/Common.py
@@ -30,3 +30,23 @@ class GuitarTunerButton(QPushButton):
         self.setObjectName(self.name)
         self.setStyleSheet("QPushButton#" + self.name + " { " + self.style + " }")
         self.setText(self.note)
+
+
+class RecordingButton(QPushButton):
+    def __init__(self, x_pos, y_pos):
+        super(RecordingButton, self).__init__()
+        self.setObjectName("Recording Button")
+        self.x = x_pos
+        self.y = y_pos
+        self.style = "border-style: solid; border-width: 1px; border-color: #c9c9c9; background-color: white; " \
+                     "border-radius: 25px; "
+        self.set_button()
+
+    def set_button(self):
+        font = QFont()
+        font.setFamilies([u"Calibri"])
+        font.setPointSize(14)
+        self.setGeometry(QRect(self.x, self.y, 50, 50))
+        self.setStyleSheet("QPushButton#" + "Recording  Button" + " { " + self.style + " }")
+        self.setText("Record")
+        self.setFont(font)
