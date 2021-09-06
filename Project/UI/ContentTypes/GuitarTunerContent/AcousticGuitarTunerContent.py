0933331ba8ad5716450dac14c01448698d995d47
Shir Avneri <shiravneri@gmail.com>
2021-09-06 17:24:42 +0300

    Fixed all guitar images

--- Project/UI/ContentTypes/GuitarTunerContent/AcousticGuitarTunerContent.py
+++ Project/UI/ContentTypes/GuitarTunerContent/AcousticGuitarTunerContent.py
@@ -9,23 +9,23 @@ class AcousticGuitarTunerContent(GuitarTunerContent):
     def __init__(self):
         super(AcousticGuitarTunerContent, self).__init__()
         self.notes = ["A", "B", "C", "D", "E", "F"]
-        self.guitar_image.setGeometry(QRect(170, 50, 260, 500))
+        self.guitar_image.setGeometry(QRect(190, 10, 220, 629))
         self.guitar_image.setStyleSheet("QLabel#GuitarImageLabel { border-image: url("
-                                        "./UI/Images/ClassicGuitarTuningPng.png) 0 0 0 stretch stretch; }")
+                                        "./UI/Images/TunerGuitars/AcousticGuitar.png) 0 0 0 stretch stretch; }")
         self.init_notes()
 
     def init_notes(self):
-        string1 = GuitarTunerButton(self.notes[0], "1", 450, 235)
+        string1 = GuitarTunerButton(self.notes[0], "1", 400, 40)
         self.notes_buttons.append(string1)
-        string2 = GuitarTunerButton(self.notes[1], "2", 450, 175)
+        string2 = GuitarTunerButton(self.notes[1], "2", 400, 95)
         self.notes_buttons.append(string2)
-        string3 = GuitarTunerButton(self.notes[2], "3", 450, 115)
+        string3 = GuitarTunerButton(self.notes[2], "3", 400, 150)
         self.notes_buttons.append(string3)
-        string4 = GuitarTunerButton(self.notes[3], "4", 100, 115)
+        string4 = GuitarTunerButton(self.notes[3], "4", 150, 150)
         self.notes_buttons.append(string4)
-        string5 = GuitarTunerButton(self.notes[4], "5", 100, 175)
+        string5 = GuitarTunerButton(self.notes[4], "5", 150, 95)
         self.notes_buttons.append(string5)
-        string6 = GuitarTunerButton(self.notes[5], "6", 100, 235)
+        string6 = GuitarTunerButton(self.notes[5], "6", 150, 40)
         self.notes_buttons.append(string6)
         for button in self.notes_buttons:
             button.setParent(self)
--- Project/UI/ContentTypes/GuitarTunerContent/ElectricGuitarTunerContent.py
+++ Project/UI/ContentTypes/GuitarTunerContent/ElectricGuitarTunerContent.py
@@ -4,28 +4,26 @@ from Project.UI.ContentTypes.GuitarTunerContent.GuitarTunerContent import Guitar
 
 
 class ElectricGuitarTunerContent(GuitarTunerContent):
-    # TO-DO:
-    # Set correct image and buttons
     def __init__(self):
         super(ElectricGuitarTunerContent, self).__init__()
         self.notes = ["1", "2", "3", "4", "5", "6"]
-        self.guitar_image.setGeometry(QRect(170, 50, 260, 500))
+        self.guitar_image.setGeometry(QRect(150, 30, 300, 630))
         self.guitar_image.setStyleSheet("QLabel#GuitarImageLabel { border-image: url("
-                                        "./UI/Images/ClassicGuitarTuningPng.png) 0 0 0 stretch stretch; }")
+                                        "./UI/Images/TunerGuitars/ElectricGuitar.png) 0 0 0 stretch stretch; }")
         self.init_notes()
 
     def init_notes(self):
-        string1 = GuitarTunerButton(self.notes[0], "1", 450, 235)
+        string1 = GuitarTunerButton(self.notes[0], "1", 285, 5)
         self.notes_buttons.append(string1)
-        string2 = GuitarTunerButton(self.notes[1], "2", 450, 175)
+        string2 = GuitarTunerButton(self.notes[1], "2", 250, 45)
         self.notes_buttons.append(string2)
-        string3 = GuitarTunerButton(self.notes[2], "3", 450, 115)
+        string3 = GuitarTunerButton(self.notes[2], "3", 215, 85)
         self.notes_buttons.append(string3)
-        string4 = GuitarTunerButton(self.notes[3], "4", 100, 115)
+        string4 = GuitarTunerButton(self.notes[3], "4", 180, 125)
         self.notes_buttons.append(string4)
-        string5 = GuitarTunerButton(self.notes[4], "5", 100, 175)
+        string5 = GuitarTunerButton(self.notes[4], "5", 145, 165)
         self.notes_buttons.append(string5)
-        string6 = GuitarTunerButton(self.notes[5], "6", 100, 235)
+        string6 = GuitarTunerButton(self.notes[5], "6", 110, 205)
         self.notes_buttons.append(string6)
         for button in self.notes_buttons:
             button.setParent(self)
