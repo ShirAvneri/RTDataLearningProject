bfa57f6c2af2883198defca2f9bc356b90887605
Shir Avneri <shiravneri@gmail.com>
2021-09-06 19:23:13 +0300

    Update ChordDetectionContent.py

--- Project/UI/ContentTypes/ChordDetection/ChordDetectionContent.py
+++ Project/UI/ContentTypes/ChordDetection/ChordDetectionContent.py
@@ -1,25 +1,16 @@
-import threading
+from PySide6.QtCore import QRect
+from PySide6.QtWidgets import QLabel, QPushButton
 
-from Project.UI.Content import Content
-from Project.UI.ContentTypes.ChordDetection.CommonClasses import RecordingButton
+# from Project.ChordDetector import chord_detection
 from Project.ChordDetector import chord_detection
+from Project.UI.Content import Content
+from Project.UI.ContentTypes.RecordingContent.CommonClasses import RecordingButton
+import threading
+import pathos.multiprocessing as multiprocessing
+from Project.UI.ContentTypes.GuitarTunerContent.CommonClasses import GuitarTunerButton
+
 
 class ChordDetectionContent(Content):
-    # def __init__(self):
-    #     super(ChordDetectionContent, self).__init__()
-    #     chord_image = QLabel(self)
-    #     chord_image.setObjectName("GuitarImageLabel")
-    #     chord_image.setGeometry(QRect(500, 50, 260, 500))
-    #     chord_image.setText("hello world")
-    #     record_button = QPushButton("record")
-    #     record_button = Recording
-    #     record_button.setGeometry(QRect(170, 50, 260, 500))
-    #     record_button.clicked.connect(self.record(record_button))
-    #
-    # def record(self, btn):
-    #     btn.setText("changed")
-    #     #chord_detection.clicked()
-
     def __init__(self):
         super(ChordDetectionContent, self).__init__()
         self.buttons = []
