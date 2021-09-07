5c835ff4bfc09a44d02746d049c183942ed4a31f
Shir Avneri <shiravneri@gmail.com>
2021-09-07 23:08:11 +0300

    Added microphone image to the recording page

--- Project/UI/ContentTypes/Recording/RecordingContent.py
+++ Project/UI/ContentTypes/Recording/RecordingContent.py
@@ -1,6 +1,7 @@
 import threading
 
-from PySide6.QtWidgets import QFileDialog
+from PySide6.QtCore import QRect
+from PySide6.QtWidgets import QFileDialog, QLabel
 
 from Project import Recording
 from Project.UI.CommonWidgets.StartStopButton import StartStopButton
@@ -12,18 +13,19 @@ class RecordingContent(Content):
         super(RecordingContent, self).__init__()
         self.is_recording = False
         self.button = StartStopButton(self.start_recording, self.stop_recording)
-        self.button.init_style("Recording Button", 500, 50)
+        self.button.init_style("Recording Button", 400, 30)
         self.button.setParent(self)
+        microphone_image = QLabel(self)
+        microphone_image.setObjectName("MicrophoneImageLabel")
+        microphone_image.setGeometry(QRect(175, 30, 250, 530))
+        microphone_image.setStyleSheet("QLabel#MicrophoneImageLabel { border-image: url(./UI/Images/Microphone.png) 0 "
+                                       "0 0 stretch stretch; }")
+        microphone_image.setParent(self)
 
     def start_recording(self):
         self.is_recording = True
         thread = threading.Thread(target=self.get_tape)
         thread.start()
-        print("after start")
-        #thread.join()
-        print("after join")
-        #thread1 = threading.Thread(target=self.save_file)
-        #thread1.start()
 
     def stop_recording(self):
         self.is_recording = False
@@ -40,12 +42,3 @@ class RecordingContent(Content):
             Recording.get_stream(stream, p, frames)
         Recording.end_stream(stream, p, frames)
         print("end get tape")
-        #fileName = QFileDialog.getSaveFileName(self, "Save F:xile",
-                                               #"/home/jana/untitled.png",
-                                              # "Images (*.png *.xpm *.jpg)")
-        #filename, _ = QFileDialog.getSaveFileName(
-         #  self,
-         #'',
-         #"ReStructuredText Files (*.rst *.txt)"
-         #)
-        #fileName = QFileDialog.getOpenFileName(None, "Select a file...", './', filter="All files (*)")
