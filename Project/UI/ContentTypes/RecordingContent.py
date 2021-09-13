a2ec46fbbf9bf6a42a78eeac30f5bcdfd9b3292c
Shir Avneri <shiravneri@gmail.com>
2021-09-13 16:25:27 +0300

    Big counter in recorder

--- Project/UI/ContentTypes/RecordingContent.py
+++ Project/UI/ContentTypes/RecordingContent.py
@@ -1,8 +1,10 @@
 import os
 import threading
 import time
-from PySide6.QtCore import QRect
-from PySide6.QtWidgets import QFileDialog, QLabel
+
+from PySide6.QtCore import QRect, Qt
+from PySide6.QtGui import QColor, QPainter, QPainterPath, QPen
+from PySide6.QtWidgets import QFileDialog, QLabel, QGraphicsDropShadowEffect, QGraphicsBlurEffect, QGraphicsScene
 
 from Project import Recording
 from Project.UI.CommonWidgets.CommonButtons import StartStopButton
@@ -29,17 +31,16 @@ class RecordingContent(Content):
 
         microphone_image = QLabel(self)
         microphone_image.setObjectName("MicrophoneImageLabel")
-        microphone_image.setGeometry(QRect(315, 30, 250, 530))
+        microphone_image.setGeometry(QRect(325, 30, 250, 530))
         microphone_image.setStyleSheet("QLabel#MicrophoneImageLabel { border-image: url(./UI/Images/Microphone.png) "
                                        "0 0 0 stretch stretch; }")
         microphone_image.setParent(self)
-
-        self.count_down_label = QLabel()
-        self.count_down_label.setParent(self)
-        self.count_down_label.setText("")
-        self.count_down_label.setGeometry(QRect(450, 300, 90, 30))
-        self.count_down_label.setFont(create_font(size=18))
-        self.count_down_label.setStyleSheet("QLabel { color : red; }");
+        self.count_down_label = QLabel(self)
+        self.count_down_label.setGeometry(QRect(0, 0, 900, 550))
+        self.count_down_label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.count_down_label.setFont(create_font(size=500, bold=True))
+        self.count_down_label.setStyleSheet("color: #972c2c")
+        self.count_down_label.hide()
 
     def start_recording(self):
         self.is_recording = True
@@ -56,20 +57,21 @@ class RecordingContent(Content):
         print("*****************")
         print(file_name[0])
         if not file_name[0]:
-            file_name = "/Grabge.py/"
+            file_name = "/Grange.py/"
             file_name = str(os.path.abspath(os.curdir)).replace("\\", "/") + file_name
         Recording.end_stream(self.stream, self.p, self.frames, file_name[0])
         self.frames = []
 
     def start_timer(self):
         self.setEnabled(False)
+        self.count_down_label.show()
         self.count_down_label.setText("3")
         time.sleep(1)
         self.count_down_label.setText("2")
         time.sleep(1)
         self.count_down_label.setText("1")
         time.sleep(1)
-        self.count_down_label.setText("")
+        self.count_down_label.hide()
         time_start = time.time()
         self.setEnabled(True)
         seconds = 0
@@ -91,9 +93,7 @@ class RecordingContent(Content):
             self.stop_recording()
 
     def save_file(self):
-        return QFileDialog.getSaveFileName(self, "Save F:xile",
-                                           "recored_file.wav",
-                                           "sound (*.wav)")
+        return QFileDialog.getSaveFileName(self, "Save F:xile", "recored_file.wav", "sound (*.wav)")
 
     def get_audio_stream(self):
         self.stream, self.p = Recording.open_stream()
