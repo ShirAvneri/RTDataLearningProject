35931da4e99a09f4e7baf347c30b42a9b26a7fca
eitan905 <eitan.sharabi@gmail.com>
2021-09-15 10:33:35 +0300

    created tester, added chords, and fixed bugs

--- Project/main.py
+++ Project/main.py
@@ -1,9 +1,15 @@
 import sys
 from PySide6.QtWidgets import QApplication
+
+from Project.Tester import Tester
 from Project.UI.AppMainWindow import AppMainWindow
 
 
 def main():
+    # tester = Tester()
+    # for i in range (20):
+    #     expected_output = tester.create_test_sample(1)
+    #     tester.execute_test(expected_output)
     app = QApplication(sys.argv)
     window = AppMainWindow()
     window.show()
