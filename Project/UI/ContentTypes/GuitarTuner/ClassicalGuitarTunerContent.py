0be16253d445c0e9ad9f1e0781ca8b8f18a14903
eitan905 <eitan.sharabi@gmail.com>
2021-09-17 13:47:24 +0300

    fixed tuner

--- Project/UI/ContentTypes/GuitarTuner/ClassicalGuitarTunerContent.py
+++ Project/UI/ContentTypes/GuitarTuner/ClassicalGuitarTunerContent.py
@@ -21,17 +21,17 @@ class ClassicalGuitarTunerContent(GuitarTunerContent):
                     button.setStyleSheet("QPushButton#" + button.name + " { " + button.style + " }")
 
     def init_notes(self):
-        string1 = GuitarTunerButton(self.notes[0], "1", 400, 55)
+        string3 = GuitarTunerButton(self.notes[3], "1", 400, 55)
+        string2 = GuitarTunerButton(self.notes[4], "2", 400, 110)
+        string1 = GuitarTunerButton(self.notes[5], "3", 400, 165)
+        string6 = GuitarTunerButton(self.notes[0], "4", 150, 165)
+        string5 = GuitarTunerButton(self.notes[1], "5", 150, 110)
+        string4 = GuitarTunerButton(self.notes[2], "6", 150, 55)
         self.notes_buttons.append(string1)
-        string2 = GuitarTunerButton(self.notes[1], "2", 400, 110)
         self.notes_buttons.append(string2)
-        string3 = GuitarTunerButton(self.notes[2], "3", 400, 165)
         self.notes_buttons.append(string3)
-        string4 = GuitarTunerButton(self.notes[3], "4", 150, 165)
         self.notes_buttons.append(string4)
-        string5 = GuitarTunerButton(self.notes[4], "5", 150, 110)
         self.notes_buttons.append(string5)
-        string6 = GuitarTunerButton(self.notes[5], "6", 150, 55)
         self.notes_buttons.append(string6)
         for button in self.notes_buttons:
             button.setParent(self)
